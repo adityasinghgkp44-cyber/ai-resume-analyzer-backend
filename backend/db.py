ef444cbbe955a7be102da66079a48885266e7f52
Aditya Kumar Singh <adityasinghgkp44@gmail.com>
2026-07-08 14:49:28 +0530

    Add Mongo URI debug

--- backend/db.py
+++ backend/db.py
@@ -5,11 +5,10 @@ import os
 load_dotenv()
 
 MONGO_URI = os.getenv("MONGO_URI")
-print("MONGO_URI =", MONGO_URI)
+print("DEBUG MONGO_URI =", MONGO_URI)
 
 client = MongoClient(MONGO_URI)
 
 db = client["ai_resume_analyzer"]
-
 users_collection = db["users"]
 resume_collection = db["resumes"]
