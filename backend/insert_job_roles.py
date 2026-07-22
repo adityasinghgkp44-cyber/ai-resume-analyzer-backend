e489eb852dd9de067440dd3b0b4ae54f2e5f0001
Aditya Kumar Singh <adityasinghgkp44@gmail.com>
2026-07-22 14:12:22 +0530

    Add register debug logs

--- backend/insert_job_roles.py
+++ backend/insert_job_roles.py
@@ -1,23 +1,34 @@
 from pymongo import MongoClient
 from job_roles import job_roles
+uri = "mongodb+srv://adityasinghgkp44_db_user:Aditya120@cluster0.wrq8om5.mongodb.net/?appName=Cluster0"
 
+print(uri)
 
+client = MongoClient(uri)
+
+print(client)
 client = MongoClient("mongodb+srv://adityasinghgkp44_db_user:Aditya120@cluster0.wrq8om5.mongodb.net/?appName=Cluster0")
 
+print("Connected!")
 
 db = client["resume_analyzer"]
-
 collection = db["job_roles"]
 
+print("DB:", db.name)
+print("Collection:", collection.name)
+
+collection.delete_many({})
 
 for role, data in job_roles.items():
-
-    document = {
+    collection.insert_one({
         "role_name": role,
         "required_skills": data["required_skills"]
-    }
+    })
 
-    collection.insert_one(document)
+print("Inserted:", collection.count_documents({}))
 
+print("Databases:", client.list_database_names())
+print("Collections:", db.list_collection_names())
 
-print("Job roles inserted successfully")
+for doc in collection.find().limit(3):
+    print(doc)
